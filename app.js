226beefd74f529c8581e905ae7c4e43a863fa279
Bartłomiej Papis <72127774+papiS23@users.noreply.github.com>
2021-07-25 01:27:50 +0200

    values obiect

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const addBtn = document.querySelector(".header__add-btn");
 const mainContainer = document.querySelector(".main");
+const mainList = document.querySelector(".main__list");
 const formContainer = document.querySelector(".form-container");
 const headerIcon = document.querySelector(".header__add-icon");
-const formDatas = document.querySelectorAll(".form-container__input");
 const formAddBtn = document.querySelector(".form-container__submit");
 
 class Job {
@@ -13,6 +13,13 @@ class Job {
     this.foil = foil;
     this.foilLength = foilLength;
     this.date = date;
+  }
+  createListItem() {
+    const Item = document.createElement("div"); //glowny kontener calego zlecenia
+
+    console.log(
+      `${this.name}---${this.date}---${this.adress}---${this.number}---${this.foilLength}---${this.foil}`
+    );
   }
 }
 
@@ -29,5 +36,15 @@ addBtn.addEventListener("click", (e) => {
 
 formAddBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log(formDatas);
+  const formDatas = document.querySelectorAll(".form-container__input");
+
+  const listItem = new Job(
+    formDatas[0].value,
+    formDatas[1].value,
+    formDatas[2].value,
+    formDatas[3].value,
+    formDatas[4].value,
+    formDatas[5].value
+  );
+  listItem.createListItem();
 });
